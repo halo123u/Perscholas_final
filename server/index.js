fb8761c2897724a203c92648bb70e35d139ed6d9
oswaldo almazo <oswaldo.almazo@gmail.com>
2017-06-12 17:23:18 -0400

    refactored code

--- server/index.js
+++ server/index.js
@@ -5,18 +5,31 @@ const request = require('request');
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-var options = {
-    url : 'https://www.haloapi.com/profile/h5/profiles/halo123u/spartan',
+
+var spartanImg, emblem;
+//Retrieves the spartan image from the Halo Api
+var userGt = (gt)=>{
+    var content = 'spartan'
+    var options = {
+    url : `https://www.haloapi.com/profile/h5/profiles/${gt}/${content}`,
     headers: {
         'Ocp-Apim-Subscription-Key': 'f02b6501d6d845dbb336dabf613d16f4'
     }
 
 }
-var spartanImg;
-request(options, (err, res, body) => {
-    spartanImg=res;
-    console.log(spartanImg);
-})
+    request(options, (err, res, body) => {
+        spartanImg=res.request.uri.href;
+        console.log(spartanImg);
+        
+    });
+    content = 'emblem';
+    options.url =  `https://www.haloapi.com/profile/h5/profiles/${gt}/${content}`;
+    console.log(options.url);     
+    request(options, (err, res, body) => {
+        console.log(emblem);
+        emblem=res.request.uri.href;
+    });
+}
 
 const publicPath = path.join(__dirname,'../public');
 app.use(express.static(publicPath));
@@ -27,9 +40,13 @@ app.get('/', (req, res)=>{
 });
 
 io.on('connection', (socket)=>{
-
-    socket.emit('news', { hello: spartanImg});
-    socket.on('my other event', (data)=>{
+    console.log('client connected');
+    socket.on('gt', (data)=>{
+        userGt(data.gt);
+        socket.emit('data', { Spartan : spartanImg,
+    Emblem: emblem});
+    });
+    socket.on('connected', (data)=>{
         console.log(data);
     })
-})
+});
