25f5fd860a7e1b500cb799ee35be25834fc16b3e
oswaldo almazo <oswaldo.almazo@gmail.com>
2017-06-12 14:05:37 -0400

    Backend set up

--- server/index.js
+++ server/index.js
@@ -1,16 +1,35 @@
 const express = require('express');
 const path = require('path');
+const request = require('request');
 
-var app = express();
-    app.use(express.static(path.join(__dirname , '../public')));
-const port = 3000;
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var options = {
+    url : 'https://www.haloapi.com/profile/h5/profiles/halo123u/spartan',
+    headers: {
+        'Ocp-Apim-Subscription-Key': 'f02b6501d6d845dbb336dabf613d16f4'
+    }
+
+}
+var spartanImg;
+request(options, (err, res, body) => {
+    spartanImg=res;
+    console.log(spartanImg);
+})
+
+const publicPath = path.join(__dirname,'../public');
+app.use(express.static(publicPath));
+server.listen(3000);
 
 app.get('/', (req, res)=>{
-    res.render('index.html');
+    res.sendFile('index.html');
+});
+
+io.on('connection', (socket)=>{
+
+    socket.emit('news', { hello: spartanImg});
+    socket.on('my other event', (data)=>{
+        console.log(data);
+    })
 })
-
-
-app.listen(port, ()=>{
-    console.log(`server is running on port ${3000}`);
-})
-
