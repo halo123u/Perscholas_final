67258e9dd73d39e34c37a91de6da845782af44f6
oswaldo almazo <oswaldo.almazo@gmail.com>
2017-06-14 16:17:52 -0400

    finished the second page

---
+++ public/js/about.js
@@ -0,0 +1,18 @@
+var slideIndex= 1;
+        showDivs(slideIndex);
+
+        function plusDivs(n){
+            showDivs(slideIndex+=n);
+        }
+        function showDivs(n){
+            var i=0;
+            var x= document.getElementsByClassName("mySlide");
+            if(n>x.length){slideIndex = 1}
+            if(n<1){slideIndex= x.length}
+            while(i<x.length){
+                x[i].style.display = "none";
+                i++;
+            }
+            x[slideIndex-1].style.display= "block";
+            
+        }
